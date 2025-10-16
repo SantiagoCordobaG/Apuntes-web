6d62f78cd79de9bcfc8caf1f254498835735aade
alexandermaya1106-hub <alexandermaya1106@gmail.com>
2025-10-15 19:50:22 -0500

    commit

--- src/views/VistaDos.vue
+++ src/views/VistaDos.vue
@@ -7,7 +7,7 @@
   import HelloWorldVue from "@/components/HelloWorld.vue";
   
   const mensajes = [
-    "Hola, soy la vista Dos, y soy maya",
+    "Hola, soy la vista Dos,soy maya y agrego un cambio ",
     "Este es un Ejemplo de mensajes"
   ];
   </script>
